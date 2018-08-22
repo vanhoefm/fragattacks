2543b19e46f6765ff0911643081b0d91536059ea
Johannes Berg <johannes.berg@intel.com>
2018-08-22 19:49:06 +0300

    random: Remove write-only variable random_entropy_file_read
    This variable is never read, so it's not needed. Remove it.
    
    Signed-off-by: Johannes Berg <johannes.berg@intel.com>

--- src/crypto/random.c
+++ src/crypto/random.c
@@ -54,7 +54,6 @@ static int random_fd = -1;
 static unsigned int own_pool_ready = 0;
 #define RANDOM_ENTROPY_SIZE 20
 static char *random_entropy_file = NULL;
-static int random_entropy_file_read = 0;
 
 #define MIN_COLLECT_ENTROPY 1000
 static unsigned int entropy = 0;
@@ -357,7 +356,6 @@ static void random_read_entropy(void)
 
 	own_pool_ready = (u8) buf[0];
 	random_add_randomness(buf + 1, RANDOM_ENTROPY_SIZE);
-	random_entropy_file_read = 1;
 	os_free(buf);
 	wpa_printf(MSG_DEBUG, "random: Added entropy from %s "
 		   "(own_pool_ready=%u)",
