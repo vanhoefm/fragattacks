9ce2015a974a6fd8632db42e438f3fc3e925455c
Jouni Malinen <jouni@codeaurora.org>
2018-12-08 01:11:44 +0200

    HS 2.0: Allocate enough buffer for HS 2.0 Indication element for scan
    The HS 2.0 Indication element can be up to 9 octets in length, so add
    two more octets to the minimum extra_ie buffer size for scanning.
    
    Signed-off-by: Jouni Malinen <jouni@codeaurora.org>

--- wpa_supplicant/scan.c
+++ wpa_supplicant/scan.c
@@ -581,7 +581,7 @@ static struct wpabuf * wpa_supplicant_extra_ies(struct wpa_supplicant *wpa_s)
 #endif /* CONFIG_WPS */
 
 #ifdef CONFIG_HS20
-	if (wpa_s->conf->hs20 && wpabuf_resize(&extra_ie, 7) == 0)
+	if (wpa_s->conf->hs20 && wpabuf_resize(&extra_ie, 9) == 0)
 		wpas_hs20_add_indication(extra_ie, -1, 0);
 #endif /* CONFIG_HS20 */
 
