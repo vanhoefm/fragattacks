837e36583a0003dd1eb33b2cc510d882cc487862
Andrey Utkin <andrey_utkin@gentoo.org>
2018-12-11 17:41:10 +0000

    Fix build with LibreSSL
    When using LibreSSL instead of OpenSSL, linkage of hostapd executable
    fails with the following error when using some LibreSSL versions
    
        ../src/crypto/tls_openssl.o: In function `tls_verify_cb':
        tls_openssl.c:(.text+0x1273): undefined reference to `ASN1_STRING_get0_data'
        ../src/crypto/tls_openssl.o: In function `tls_connection_peer_serial_num':
        tls_openssl.c:(.text+0x3023): undefined reference to `ASN1_STRING_get0_data'
        collect2: error: ld returned 1 exit status
        make: *** [Makefile:1278: hostapd] Error 1
    
    ASN1_STRING_get0_data is present in recent OpenSSL, but absent in some
    versions of LibreSSL (confirmed for version 2.6.5), so fallback needs to
    be defined in this case, just like for old OpenSSL.
    
    This patch was inspired by similar patches to other projects, such as
    spice-gtk, pjsip.
    
    Link: https://bugs.gentoo.org/672834
    Signed-off-by: Andrey Utkin <andrey_utkin@gentoo.org>

--- src/crypto/tls_openssl.c
+++ src/crypto/tls_openssl.c
@@ -104,7 +104,9 @@ static size_t SSL_SESSION_get_master_key(const SSL_SESSION *session,
 
 #endif
 
-#if OPENSSL_VERSION_NUMBER < 0x10100000L
+#if OPENSSL_VERSION_NUMBER < 0x10100000L || \
+	(defined(LIBRESSL_VERSION_NUMBER) && \
+	 LIBRESSL_VERSION_NUMBER < 0x20700000L)
 #ifdef CONFIG_SUITEB
 static int RSA_bits(const RSA *r)
 {
