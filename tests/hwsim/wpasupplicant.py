torvalds/linux

commit 113f3aaa81bd56aba02659786ed65cbd9cb9a6fc

cfg80211: Prevent regulatory restore during STA disconnect in concurrent interfaces
Currently when an AP and STA interfaces are active in the same or different
radios, regulatory settings are restored whenever the STA disconnects. This
restores all channel information including dfs states in all radios.
For example, if an AP interface is active in one radio and STA in another,
when radar is detected on the AP interface, the dfs state of the channel
will be changed to UNAVAILABLE. But when the STA interface disconnects,
this issues a regulatory disconnect hint which restores all regulatory
settings in all the radios attached and thereby losing the stored dfs
state on the other radio where the channel was marked as unavailable
earlier. Hence prevent such regulatory restore whenever another active
beaconing interface is present in the same or other radios.

Signed-off-by: Sriram R <srirrama@codeaurora.org>
Signed-off-by: Johannes Berg <johannes.berg@intel.com>

net/wireless/sme.c
@@ -642,11 +642,15 @@ static bool cfg80211_is_all_idle(void)
 	 * All devices must be idle as otherwise if you are actively
 	 * scanning some new beacon hints could be learned and would
 	 * count as new regulatory hints.
+	 * Also if there is any other active beaconing interface we
+	 * need not issue a disconnect hint and reset any info such
+	 * as chan dfs state, etc.
 	 */
 	list_for_each_entry(rdev, &cfg80211_rdev_list, list) {
 		list_for_each_entry(wdev, &rdev->wiphy.wdev_list, list) {
 			wdev_lock(wdev);
-			if (wdev->conn || wdev->current_bss)
+			if (wdev->conn || wdev->current_bss ||
+			    cfg80211_beaconing_iface_active(wdev))
 				is_all_idle = false;
 			wdev_unlock(wdev);
 		}

vanhoefm/fragattacks

commit 99bc57d0d1b14218d9d4767aa0ea33b2d0120f78

tests: Fix mbo_supp_oper_classes after cfg80211 change
The Linux kernel commit 113f3aaa81bd ("cfg80211: Prevent regulatory
restore during STA disconnect in concurrent interfaces") broke the
regulatory clearing attempt in this test case since
cfg80211_is_all_idle() is now returning false due to the AP interface
being up and that results in the Country IE -based regulatory
information not getting cleared back to defaults.

Work around this by stopping the AP interface first so that when the
station interface receives the disconnection, there are no other active
interfaces in the system. In addition, wait for REGDOM event for the
Country IE hint after association to avoid disconnection before the
regulatory events have been fully processed.

Signed-off-by: Jouni Malinen <j@w1.fi>

tests/hwsim/test_mbo.py
@@ -35,6 +35,7 @@ def run_mbo_supp_oper_classes(dev, apdev, hapd, hapd2, country):
     dev[0].dump_monitor()
 
     logger.info("Country: " + country)
+    dev[0].note("Setting country code " + country)
     set_reg(country, apdev[0], apdev[1], dev[0])
     for j in range(5):
         ev = dev[0].wait_event(["CTRL-EVENT-REGDOM-CHANGE"], timeout=5)
@@ -52,8 +53,10 @@ def run_mbo_supp_oper_classes(dev, apdev, hapd, hapd2, country):
         dev[0].connect("test-wnm-mbo", key_mgmt="NONE", scan_freq="5180")
         sta = hapd.get_sta(addr)
         res5 = sta['supp_op_classes'][2:]
-        dev[0].request("REMOVE_NETWORK all")
+        dev[0].wait_regdom(country_ie=True)
         hapd.disable()
+        dev[0].request("REMOVE_NETWORK all")
+        dev[0].request("ABORT_SCAN")
         dev[0].wait_disconnected()
         dev[0].dump_monitor()
 
@@ -63,8 +66,10 @@ def run_mbo_supp_oper_classes(dev, apdev, hapd, hapd2, country):
     dev[0].connect("test-wnm-mbo-2", key_mgmt="NONE", scan_freq="2412")
     sta = hapd2.get_sta(addr)
     res2 = sta['supp_op_classes'][2:]
-    dev[0].request("REMOVE_NETWORK all")
+    dev[0].wait_regdom(country_ie=True)
     hapd2.disable()
+    dev[0].request("REMOVE_NETWORK all")
+    dev[0].request("ABORT_SCAN")
     dev[0].wait_disconnected()
     dev[0].dump_monitor()
 
@@ -91,6 +96,7 @@ def test_mbo_supp_oper_classes(dev, apdev):
     hapd2 = hostapd.add_ap(apdev[1], params, no_enable=True)
 
     try:
+        dev[0].request("STA_AUTOCONNECT 0")
         za2, za5 = run_mbo_supp_oper_classes(dev, apdev, hapd, hapd2, "ZA")
         fi2, fi5 = run_mbo_supp_oper_classes(dev, apdev, hapd, hapd2, "FI")
         us2, us5 = run_mbo_supp_oper_classes(dev, apdev, hapd, hapd2, "US")
@@ -101,6 +107,7 @@ def test_mbo_supp_oper_classes(dev, apdev):
         dev[0].dump_monitor()
         set_reg("00", apdev[0], apdev[1], dev[0])
         ev = dev[0].wait_event(["CTRL-EVENT-REGDOM-CHANGE"], timeout=1)
+        dev[0].request("STA_AUTOCONNECT 1")
 
     za = "515354737475767778797a7b808182"
     fi = "515354737475767778797a7b808182"

tests/hwsim/wpasupplicant.py
@@ -1319,3 +1319,14 @@ class WpaSupplicant:
 
     def note(self, txt):
         self.request("NOTE " + txt)
+
+    def wait_regdom(self, country_ie=False):
+        for i in range(5):
+            ev = self.wait_event(["CTRL-EVENT-REGDOM-CHANGE"], timeout=1)
+            if ev is None:
+                break
+            if country_ie:
+                if "init=COUNTRY_IE" in ev:
+                    break
+            else:
+                break