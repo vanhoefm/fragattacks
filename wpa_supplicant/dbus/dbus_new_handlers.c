441ba1a3e34b310cb57d87d0979fe4379f07815a
Jouni Malinen <j@w1.fi>
2019-01-05 18:42:22 +0200

    dbus: Fix build without CONFIG_WNM=y
    wpa_s->bss_tm_status is within #ifdef CONFIG_WNM, so need to access it
    through matching condition.
    
    Fixes: 80d06d0ca9f3 ("dbus: Export BSS Transition Management status")
    Signed-off-by: Jouni Malinen <j@w1.fi>

--- wpa_supplicant/dbus/dbus_new_handlers.c
+++ wpa_supplicant/dbus/dbus_new_handlers.c
@@ -3213,8 +3213,12 @@ dbus_bool_t wpas_dbus_getter_bss_tm_status(
 	const struct wpa_dbus_property_desc *property_desc,
 	DBusMessageIter *iter, DBusError *error, void *user_data)
 {
+#ifdef CONFIG_WNM
 	struct wpa_supplicant *wpa_s = user_data;
 	dbus_uint32_t bss_tm_status = wpa_s->bss_tm_status;
+#else /* CONFIG_WNM */
+	dbus_uint32_t bss_tm_status = 0;
+#endif /* CONFIG_WNM */
 
 	return wpas_dbus_simple_property_getter(iter, DBUS_TYPE_UINT32,
 						&bss_tm_status, error);
