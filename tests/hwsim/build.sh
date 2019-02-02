00bd8157aa3d3345f9f373577fbcc978b7ede451
Johannes Berg <johannes.berg@intel.com>
2019-02-02 23:16:05 +0100

    tests: Build hs20-osu-client
    For tests, build the HS 2.0 OSU client (without browser to avoid
    having webkit/curl dependencies).
    
    Signed-off-by: Johannes Berg <johannes.berg@intel.com>

--- tests/hwsim/build.sh
+++ tests/hwsim/build.sh
@@ -36,8 +36,13 @@ cd ../../../wlantest
 make clean > /dev/null
 make QUIET=1 -j8 > /dev/null
 
+echo "Building hs20-osu-client"
+cd ../hs20/client/
+make clean > /dev/null 2>&1
+make QUIET=1 CONFIG_NO_BROWSER=1
+
 echo "Building hostapd"
-cd ../hostapd
+cd ../../hostapd
 if [ ! -e .config -o $force_config -eq 1 ]; then
     cp ../tests/hwsim/example-hostapd.config .config
 else
