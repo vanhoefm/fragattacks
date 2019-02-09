e5bffe1aa9e7324142bb70f6939487afd185ad31
Jouni Malinen <j@w1.fi>
2019-02-09 23:58:58 +0200

    OpenSSL: Add more handshake message names to debug
    Signed-off-by: Jouni Malinen <j@w1.fi>

--- src/crypto/tls_openssl.c
+++ src/crypto/tls_openssl.c
@@ -1347,8 +1347,16 @@ static const char * openssl_handshake_type(int content_type, const u8 *buf,
 		return "client hello";
 	case 2:
 		return "server hello";
+	case 3:
+		return "hello verify request";
 	case 4:
 		return "new session ticket";
+	case 5:
+		return "end of early data";
+	case 6:
+		return "hello retry request";
+	case 8:
+		return "encrypted extensions";
 	case 11:
 		return "certificate";
 	case 12:
@@ -1367,6 +1375,12 @@ static const char * openssl_handshake_type(int content_type, const u8 *buf,
 		return "certificate url";
 	case 22:
 		return "certificate status";
+	case 23:
+		return "supplemental data";
+	case 24:
+		return "key update";
+	case 254:
+		return "message hash";
 	default:
 		return "?";
 	}
