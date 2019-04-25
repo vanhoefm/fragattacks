71da22b94b625ba4da45d7de3f3e384c04fe433e
Jouni Malinen <jouni@codeaurora.org>
2019-04-26 00:18:52 +0300

    tests: Initial and still very minimal HE testing
    Signed-off-by: Jouni Malinen <jouni@codeaurora.org>

---
+++ tests/hwsim/test_he.py
@@ -0,0 +1,17 @@
+# HE tests
+# Copyright (c) 2019, The Linux Foundation
+#
+# This software may be distributed under the terms of the BSD license.
+# See README for more details.
+
+import hostapd
+
+def test_he_open(dev, apdev):
+    """HE AP with open mode configuration"""
+    params = {"ssid": "he",
+              "ieee80211ax": "1",
+              "he_bss_color": "42",
+              "he_mu_edca_ac_be_ecwmin": "7",
+              "he_mu_edca_ac_be_ecwmax": "15"}
+    hapd = hostapd.add_ap(apdev[0], params)
+    dev[0].connect("he", key_mgmt="NONE", scan_freq="2412")
