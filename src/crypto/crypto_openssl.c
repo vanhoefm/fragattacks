vanhoefm/krackattacks-scripts

commit f29761297b849311384a1dd4ebcec250b2fd4471

BoringSSL: Implement crypto_ecdh_init()
BoringSSL does not provide some of the OpenSSL API that was used here,
so update this to use similar design to what was already done with DPP
key derivation.

Signed-off-by: Jouni Malinen <jouni@qca.qualcomm.com>

src/crypto/crypto_openssl.c
@@ -1701,7 +1701,12 @@ struct crypto_ecdh * crypto_ecdh_init(int group)
 {
 	struct crypto_ecdh *ecdh;
 	EVP_PKEY *params = NULL;
+#ifdef OPENSSL_IS_BORINGSSL
+	const EC_GROUP *ec_group;
+	EC_KEY *ec_params;
+#else /* OPENSSL_IS_BORINGSSL */
 	EVP_PKEY_CTX *pctx = NULL;
+#endif /* OPENSSL_IS_BORINGSSL */
 	EVP_PKEY_CTX *kctx = NULL;
 
 	ecdh = os_zalloc(sizeof(*ecdh));
@@ -1712,6 +1717,22 @@ struct crypto_ecdh * crypto_ecdh_init(int group)
 	if (!ecdh->ec)
 		goto fail;
 
+#ifdef OPENSSL_IS_BORINGSSL
+	ec_group = EC_GROUP_new_by_curve_name(ecdh->ec->nid);
+	ec_params = EC_KEY_new();
+	if (!ec_params || EC_KEY_set_group(ec_params, ec_group) != 1) {
+		wpa_printf(MSG_ERROR,
+			   "BoringSSL: Failed to generate EC_KEY parameters");
+		goto fail;
+	}
+	EC_KEY_set_asn1_flag(ec_params, OPENSSL_EC_NAMED_CURVE);
+	params = EVP_PKEY_new();
+	if (!params || EVP_PKEY_set1_EC_KEY(params, ec_params) != 1) {
+		wpa_printf(MSG_ERROR,
+			   "BoringSSL: Failed to generate EVP_PKEY parameters");
+		goto fail;
+	}
+#else /* OPENSSL_IS_BORINGSSL */
 	pctx = EVP_PKEY_CTX_new_id(EVP_PKEY_EC, NULL);
 	if (!pctx)
 		goto fail;
@@ -1735,6 +1756,7 @@ struct crypto_ecdh * crypto_ecdh_init(int group)
 			   ERR_error_string(ERR_get_error(), NULL));
 		goto fail;
 	}
+#endif /* OPENSSL_IS_BORINGSSL */
 
 	kctx = EVP_PKEY_CTX_new(params, NULL);
 	if (!kctx)
@@ -1755,7 +1777,9 @@ struct crypto_ecdh * crypto_ecdh_init(int group)
 
 done:
 	EVP_PKEY_free(params);
+#ifndef OPENSSL_IS_BORINGSSL
 	EVP_PKEY_CTX_free(pctx);
+#endif /* OPENSSL_IS_BORINGSSL */
 	EVP_PKEY_CTX_free(kctx);
 
 	return ecdh;

vanhoefm/fragattacks

commit f9cd1327adf723035d5b2d73e41fc8b9e865ccbd

OpenSSL: Fix memory leak in crypto_ecdh_init()
ec_params needs to be freed before returning from the function.
Extension of this function to support BoringSSL introduced this memory
leak and that was later extended to be the only variant and apply to
OpenSSL and LibreSSL cases as well in commit c23e87d0d12d ("OpenSSL:
Replace EVP_PKEY_paramgen() with EC_KEY_new_by_curve_name()").

Fixes: f29761297b84 ("BoringSSL: Implement crypto_ecdh_init()")
Signed-off-by: Jouni Malinen <jouni@codeaurora.org>

src/crypto/crypto_openssl.c
@@ -1880,7 +1880,7 @@ struct crypto_ecdh * crypto_ecdh_init(int group)
 {
 	struct crypto_ecdh *ecdh;
 	EVP_PKEY *params = NULL;
-	EC_KEY *ec_params;
+	EC_KEY *ec_params = NULL;
 	EVP_PKEY_CTX *kctx = NULL;
 
 	ecdh = os_zalloc(sizeof(*ecdh));
@@ -1923,6 +1923,7 @@ struct crypto_ecdh * crypto_ecdh_init(int group)
 	}
 
 done:
+	EC_KEY_free(ec_params);
 	EVP_PKEY_free(params);
 	EVP_PKEY_CTX_free(kctx);
 