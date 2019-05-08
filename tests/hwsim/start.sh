7153540c39ca256739a6c66f90e8a9e552154062
Jouni Malinen <jouni@codeaurora.org>
2019-05-08 13:07:50 +0300

    tests: Enable full memory leak reporting in valgrind
    This has heavier CPU and memory requirements, but is needed to report
    memory leaks in libraries.
    
    Signed-off-by: Jouni Malinen <jouni@codeaurora.org>

--- tests/hwsim/start.sh
+++ tests/hwsim/start.sh
@@ -80,12 +80,12 @@ fi
 
 if [ "$1" = "valgrind" ]; then
     VALGRIND=y
-    VALGRIND_WPAS="valgrind --log-file=$LOGDIR/valgrind-wlan%d"
-    VALGRIND_HAPD="valgrind --log-file=$LOGDIR/valgrind-hostapd"
+    VALGRIND_WPAS="valgrind --log-file=$LOGDIR/valgrind-wlan%d --leak-check=full"
+    VALGRIND_HAPD="valgrind --log-file=$LOGDIR/valgrind-hostapd --leak-check=full"
     chmod -f a+rx $WPAS
     chmod -f a+rx $HAPD
     chmod -f a+rx $HAPD_AS
-    HAPD_AS="valgrind --log-file=$LOGDIR/valgrind-auth-serv $HAPD_AS"
+    HAPD_AS="valgrind --log-file=$LOGDIR/valgrind-auth-serv --leak-check=full $HAPD_AS"
     shift
 else
     unset VALGRIND
