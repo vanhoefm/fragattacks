d1372751313d8e4a14e4e79db1ccd09ccffc899c
Jouni Malinen <jouni@codeaurora.org>
2019-08-06 13:21:56 +0300

    tests: Remove openssl header files from code coverage report
    Signed-off-by: Jouni Malinen <jouni@codeaurora.org>

--- tests/hwsim/vm/combine-codecov.sh
+++ tests/hwsim/vm/combine-codecov.sh
@@ -19,6 +19,8 @@ done
 lcov $args -o $LOGDIR/combined.info > $LOGDIR/combined-lcov.log 2>&1
 cat $LOGDIR/combined.info |
     sed "/^TN:$/{N;s/TN:\n\(SF:.*\/bits\/byteswap.h$\)/\1/};/^SF:.*\/bits\/byteswap.h$/,/^end_of_record$/d" |
+    sed "/^TN:$/{N;s/TN:\n\(SF:.*\/openssl\/x509.h$\)/\1/};/^SF:.*\/openssl\/x509.h$/,/^end_of_record$/d" |
+    sed "/^TN:$/{N;s/TN:\n\(SF:.*\/openssl\/x509v3.h$\)/\1/};/^SF:.*\/openssl\/x509v3.h$/,/^end_of_record$/d" |
     sed "/^TN:$/{N;s/TN:\n\(SF:.*\/common\/wpa_ctrl.c$\)/\1/};/^SF:.*\/common\/wpa_ctrl.c$/,/^end_of_record$/d" |
     sed "/^TN:$/{N;s/TN:\n\(SF:.*\/common\/cli.c$\)/\1/};/^SF:.*\/common\/cli.c$/,/^end_of_record$/d" |
     sed "/^TN:$/{N;s/TN:\n\(SF:.*\/utils\/edit.c$\)/\1/};/^SF:.*\/utils\/edit.c$/,/^end_of_record$/d" |
