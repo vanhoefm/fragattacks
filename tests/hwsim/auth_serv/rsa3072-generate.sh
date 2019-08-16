eec147dccb792a50fb0791ea8f1fd20326a55313
Jouni Malinen <jouni@codeaurora.org>
2019-08-16 16:21:38 +0300

    tests: Update RSA 3k certificates before the previous ones expire
    In addition, update the generation script to allow convenient update of
    the server and user certificates without having to generate new keys.
    
    Signed-off-by: Jouni Malinen <jouni@codeaurora.org>

--- tests/hwsim/auth_serv/rsa3072-generate.sh
+++ tests/hwsim/auth_serv/rsa3072-generate.sh
@@ -38,7 +38,9 @@ cat ec-ca-openssl.cnf |
 	sed "s/#@ALTNAME@/subjectAltName=critical,DNS:rsa3072.server.w1.fi/" |
 	sed s%\./ec-ca$%./rsa3072-ca% \
 	> rsa3072-ca-openssl.cnf.tmp
-$OPENSSL req -config rsa3072-ca-openssl.cnf.tmp -batch -new -newkey rsa:3072 -nodes -keyout rsa3072-server.key -out rsa3072-server.req -outform PEM -sha384
+if [ ! -r rsa3072-server.req ]; then
+    $OPENSSL req -config rsa3072-ca-openssl.cnf.tmp -batch -new -newkey rsa:3072 -nodes -keyout rsa3072-server.key -out rsa3072-server.req -outform PEM -sha384
+fi
 $OPENSSL ca -config rsa3072-ca-openssl.cnf.tmp -batch -keyfile rsa3072-ca.key -cert rsa3072-ca.pem -create_serial -in rsa3072-server.req -out rsa3072-server.pem -extensions ext_server -days 730 -md sha384
 rm rsa3072-ca-openssl.cnf.tmp
 
@@ -51,7 +53,9 @@ cat ec-ca-openssl.cnf |
 	sed "s/#@ALTNAME@/subjectAltName=email:user-rsa3072@w1.fi/" |
 	sed s%\./ec-ca$%./rsa3072-ca% \
 	> rsa3072-ca-openssl.cnf.tmp
-$OPENSSL req -config rsa3072-ca-openssl.cnf.tmp -batch -new -newkey rsa:3072 -nodes -keyout rsa3072-user.key -out rsa3072-user.req -outform PEM -extensions ext_client -sha384
+if [ ! -r rsa3072-user.req ]; then
+    $OPENSSL req -config rsa3072-ca-openssl.cnf.tmp -batch -new -newkey rsa:3072 -nodes -keyout rsa3072-user.key -out rsa3072-user.req -outform PEM -extensions ext_client -sha384
+fi
 $OPENSSL ca -config rsa3072-ca-openssl.cnf.tmp -batch -keyfile rsa3072-ca.key -cert rsa3072-ca.pem -create_serial -in rsa3072-user.req -out rsa3072-user.pem -extensions ext_client -days 730 -md sha384
 rm rsa3072-ca-openssl.cnf.tmp
 
@@ -64,7 +68,9 @@ cat ec-ca-openssl.cnf |
 	sed "s/#@ALTNAME@/subjectAltName=email:user-rsa3072-rsa2048@w1.fi/" |
 	sed s%\./ec-ca$%./rsa3072-ca% \
 	> rsa3072-ca-openssl.cnf.tmp
-$OPENSSL req -config rsa3072-ca-openssl.cnf.tmp -batch -new -newkey rsa:2048 -nodes -keyout rsa3072-user-rsa2048.key -out rsa3072-user-rsa2048.req -outform PEM -extensions ext_client -sha384
+if [ ! -r rsa3072-user-rsa2048.req ]; then
+    $OPENSSL req -config rsa3072-ca-openssl.cnf.tmp -batch -new -newkey rsa:2048 -nodes -keyout rsa3072-user-rsa2048.key -out rsa3072-user-rsa2048.req -outform PEM -extensions ext_client -sha384
+fi
 $OPENSSL ca -config rsa3072-ca-openssl.cnf.tmp -batch -keyfile rsa3072-ca.key -cert rsa3072-ca.pem -create_serial -in rsa3072-user-rsa2048.req -out rsa3072-user-rsa2048.pem -extensions ext_client -days 730 -md sha384
 rm rsa3072-ca-openssl.cnf.tmp
 
