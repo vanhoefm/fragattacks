be4d268a604e4bd1fa49c3c1382d024b4bd857a3
Mathy Vanhoef <Mathy.Vanhoef@nyu.edu>
2020-06-26 09:59:18 +0400

    fragattack: improvements to injection tests

--- research/test-injection.py
+++ research/test-injection.py
@@ -8,21 +8,31 @@ def main():
 	parser.add_argument('monitor', nargs='?', help="Interface to use to monitor for frames.")
 	options = parser.parse_args()
 
+	peermac = "00:11:22:33:44:55"
+
 	subprocess.check_output(["rfkill", "unblock", "wifi"])
 
 	set_monitor_mode(options.inject)
 	if options.monitor:
 		set_monitor_mode(options.monitor)
-		if get_channel(options.inject) != get_channel(options.monitor):
+		chan_inject = get_channel(options.inject)
+		chan_monitor = get_channel(options.monitor)
+		if chan_inject == None or chan_monitor == None:
+			log(WARNING, "Unable to verify if both devices are on the same channel")
+		elif chan_inject != chan_monitor:
 			log(ERROR, "Both devices are not on the same channel")
 			quit(1)
+		peermac = get_mac_address(options.monitor)
 	else:
 		log(WARNING, "Only performing selftest. This can detect only injection issues caused by")
-		log(WARNING, "the kernel. Many other issues cannot be detected in this self-test,so you")
+		log(WARNING, "the kernel. Many other issues cannot be detected in this self-test, so you")
 		log(WARNING, "should not trust the output of the tests unless you know what you're doing.")
 
 	log(STATUS, "Performing injection tests ...")
-	test_injection(options.inject, options.monitor, peermac="00:11:22:33:44:55")
+	try:
+		test_injection(options.inject, options.monitor, peermac)
+	except OSError as ex:
+		log(ERROR, str(ex))
 
 if __name__ == "__main__":
 	main()
