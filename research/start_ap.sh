d7c60cdb396166228668a3c4094b8e4f13eb7f1b
Mathy Vanhoef <Mathy.Vanhoef@kuleuven.be>
2020-07-26 13:03:26 +0400

    fragattacks: remove or move some scripts

--- research/start_ap.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-set -e
-
-../hostapd/hostapd ../hostapd/hostapd.conf -dd -K
--- research/start_sta.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-set -e
-
-../wpa_supplicant/wpa_supplicant -D nl80211 -i wlan1 -c client.conf -dd -K
