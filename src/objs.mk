vanhoefm/fragattacks

commit 87098d3324e018fd05c1729998052603391a4e71

build: Put archive files into build/ folder too
This is something I hadn't previously done, but there are
cases where it's needed, e.g., building 'wlantest' and then
one of the tests/fuzzing/*/ projects, they use a different
configuration (fuzzing vs. not fuzzing).

Perhaps more importantly, this gets rid of the last thing
that was dumped into the source directories, apart from
the binaries themselves.

Note that due to the use of thin archives, this required
building with absolute paths.

Signed-off-by: Johannes Berg <johannes.berg@intel.com>

src/objs.mk
@@ -1,3 +1,3 @@
 $(_OBJS_VAR) := $(call BUILDOBJ,$($(_OBJS_VAR)))
--include $($(_OBJS_VAR):%.o=%.d)
+-include $($($(_OBJS_VAR):%.o=%.d):%.a=%.d)
 _DIRS += $(dir $($(_OBJS_VAR)))

eap_example/Makefile
@@ -90,15 +90,8 @@ OBJS_ex = eap_example.o eap_example_peer.o eap_example_server.o
 _OBJS_VAR := OBJS_ex
 include ../src/objs.mk
 
-
-../src/utils/libutils.a:
-	$(MAKE) -C ../src/utils
-
-../src/crypto/libcrypto.a:
-	$(MAKE) -C ../src/crypto
-
-../src/tls/libtls.a:
-	$(MAKE) -C ../src/tls
+_OBJS_VAR := EAP_LIBS
+include ../src/objs.mk
 
 
 ifneq ($(CONFIG_SOLIB), yes)

radius_example/Makefile
@@ -11,20 +11,14 @@ LIBS += ../src/crypto/libcrypto.a
 LIBS += ../src/utils/libutils.a
 LLIBS = -lrt
 
-../src/utils/libutils.a:
-	$(MAKE) -C ../src/utils
-
-../src/crypto/libcrypto.a:
-	$(MAKE) -C ../src/crypto
-
-../src/radius/libradius.a:
-	$(MAKE) -C ../src/radius
-
 #CLAGS += -DCONFIG_IPV6
 
 OBJS_ex = radius_example.o
 
 _OBJS_VAR := OBJS_ex
+include ../src/objs.mk
+
+_OBJS_VAR := LIBS
 include ../src/objs.mk
 
 radius_example: $(OBJS_ex) $(LIBS)

src/ap/Makefile
@@ -1,4 +1,4 @@
-ALL=libap.a
+ALL=$(OUT)libap.a
 
 include ../lib.rules
 
@@ -67,5 +67,5 @@ LIB_OBJS= \
 
 _OBJS_VAR := LIB_OBJS
 include ../objs.mk
-libap.a: $(LIB_OBJS)
+$(OUT)libap.a: $(LIB_OBJS)
 	$(AR) crT $@ $?

src/build.rules
@@ -9,9 +9,14 @@ ROOTDIR := $(dir $(lastword $(MAKEFILE_LIST)))
 ROOTDIR := $(dir $(ROOTDIR:%../src/=%))../
 BUILDDIR ?= $(abspath $(ROOTDIR)build)
 BUILDDIR := $(BUILDDIR:%/=%)
-_PROJ := $(abspath $(dir $(firstword $(MAKEFILE_LIST))))
 ABSROOT := $(abspath $(ROOTDIR))
+ifeq ($(origin OUT),command line)
+_PROJ := $(OUT:%/=%)
+_PROJ := $(_PROJ:$(BUILDDIR)/%=%)
+else
+_PROJ := $(abspath $(dir $(firstword $(MAKEFILE_LIST))))
 _PROJ := $(_PROJ:$(ABSROOT)/%=%)
+endif
 
 ifndef CC
 CC=gcc
@@ -88,9 +93,13 @@ $(BUILDDIR)/$(PROJ)/wpa_supplicant/%.o: $(ROOTDIR)wpa_supplicant/%.c $(CONFIG_FI
 	$(Q)$(CC) -c -o $@ $(CFLAGS) $<
 	@$(E) "  CC " $<
 
+# libraries - they know how to build themselves
+$(BUILDDIR)/$(PROJ)/%.a: $(CONFIG_FILE)
+	$(Q)mkdir -p $(BUILDDIR)/$(PROJ)/$(dir $(@:$(BUILDDIR)/$(PROJ)/%=%))
+	$(Q)$(MAKE) -C $(ROOTDIR)$(dir $(@:$(BUILDDIR)/$(PROJ)/%=%)) OUT=$(abspath $(dir $@))/
+
 BUILDOBJ = $(patsubst %,$(BUILDDIR)/$(PROJ)/%,$(patsubst $(ROOTDIR)%,%,$(1)))
 
 .PHONY: common-clean
 common-clean:
-	$(Q)$(MAKE) -C $(ROOTDIR)/src clean
 	$(Q)rm -rf $(ALL) $(BUILDDIR)/$(PROJ)

src/common/Makefile
@@ -1,4 +1,4 @@
-ALL=libcommon.a
+ALL=$(OUT)libcommon.a
 
 include ../lib.rules
 
@@ -21,5 +21,5 @@ LIB_OBJS= \
 
 _OBJS_VAR := LIB_OBJS
 include ../objs.mk
-libcommon.a: $(LIB_OBJS)
+$(OUT)libcommon.a: $(LIB_OBJS)
 	$(AR) crT $@ $?

src/crypto/Makefile
@@ -1,4 +1,4 @@
-ALL=libcrypto.a
+ALL=$(OUT)libcrypto.a
 
 include ../lib.rules
 
@@ -68,5 +68,5 @@ endif
 
 _OBJS_VAR := LIB_OBJS
 include ../objs.mk
-libcrypto.a: $(LIB_OBJS)
+$(OUT)libcrypto.a: $(LIB_OBJS)
 	$(AR) crT $@ $?

src/eap_common/Makefile
@@ -1,4 +1,4 @@
-ALL=libeap_common.a
+ALL=$(OUT)libeap_common.a
 
 include ../lib.rules
 
@@ -25,5 +25,5 @@ LIB_OBJS= \
 
 _OBJS_VAR := LIB_OBJS
 include ../objs.mk
-libeap_common.a: $(LIB_OBJS)
+$(OUT)libeap_common.a: $(LIB_OBJS)
 	$(AR) crT $@ $?

commit 154b18d950588c463806ac5b80ade189ea8eb9e7

build: Fix dependency file inclusion
The objs.mk include changes for archive files broke things
completely and none of the dependency files (*.d) ever got
included, as the expansion there ended up empty.

Clearly, my mistake, I should've tested that better. As we
don't need the %.a files in the list there use filter-out
to remove them, rather than what I had lazily wanted to do,
which was trying to read %.d files for them. The filter-out
actually works, and avoids looking up files that can never
exist in the first place.

Fixes: 87098d3324e0 ("build: Put archive files into build/ folder too")
Signed-off-by: Johannes Berg <johannes.berg@intel.com>

src/objs.mk
@@ -1,3 +1,3 @@
 $(_OBJS_VAR) := $(call BUILDOBJ,$($(_OBJS_VAR)))
--include $($($(_OBJS_VAR):%.o=%.d):%.a=%.d)
+-include $(filter-out %.a,$($(_OBJS_VAR):%.o=%.d))
 _DIRS += $(dir $($(_OBJS_VAR)))